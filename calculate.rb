7e0598ed78e638275d24d04154d01445cd236cfb
haisong <haisong95@gmail.com>
2013-10-31 13:51:20 -0500

    sortmethod

---
+++ calculate.rb
@@ -0,0 +1,24 @@
+def calculate_total cards
+	arr = cards.map{|e| e[1]}
+
+	total = 0
+	arr.each do |value|
+		if value == "A"
+			 total += 11      #Ruby is read descendly. write if in correct order!
+			
+		elsif value.to_i == 0
+			 total += 10
+		else
+			total += value.to_i
+		end
+	end
+
+	arr.select{|e| e == "A"}.count.times do
+		total -= 10 if total > 21 
+	end
+
+	total
+end
+
+a = [["S","J"], ["S","A"],["D","7"]]
+puts calculate_total(a)
